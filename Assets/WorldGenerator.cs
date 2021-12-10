83e3d833676d316a15a0e0304678bf40cf149faf
tyrel <tyrel.ratima@gmail.com>
2021-12-10 15:16:16 +1300

    Base game
    Base Game Tings

---
+++ Assets/WorldGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WorldGenerator : MonoBehaviour
+{
+    public GameObject[] TileGen;
+
+    public int iterations = 20;
+    float offset = 10.5f;
+
+    void Generate()
+    {
+
+        for(int i = iterations; i >0; i--)
+        {
+            int selection = Random.Range(0, TileGen.Length);
+            Vector3 spawnPos = new Vector3(i * offset, 0, 0);
+            Instantiate(TileGen[selection], transform.position + spawnPos, Quaternion.identity);
+        }
+
+    }
+
+    void Start()
+    {
+        Generate();
+    }
+
+    
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    public int extraJumps;
+    public int extraJumpsValue;
+
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    public bool isGrounded;
+    public Transform groundCheck;
+    public float checkRadius;
+    public LayerMask WhatIsGound;
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        extraJumps = extraJumpsValue;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, checkRadius, WhatIsGound);
+
+        moveInput = Input.GetAxis("Horizontal");
+        Debug.Log(moveInput);
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        if(facingRight == false && moveInput > 0)
+        {
+            Flip();
+        } else if(facingRight == true && moveInput < 0)
+        {
+            Flip();
+        }
+
+    }
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+    }
+
+    void Update()
+    {
+
+        if(isGrounded == true)
+        {
+            extraJumps = 1;
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+            extraJumps--;
+        } else if(Input.GetKeyDown(KeyCode.Space) && extraJumps > 0 && isGrounded == true)
+        {
+            rb.velocity = Vector2.up * jumpForce;
+        }
+
+    }
+
+}
